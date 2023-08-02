fdfd7d5756c16729e25f83333f4214a0928c3efd
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-02 14:05:57 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     $(window).on('action:ajaxify.end', function (evt, data) {
-        if(data.tpl_url === 'register') {
-            socket.emit('plugins.mathCaptcha.getProblem', function (err, results) {
+        if(data.tpl_url === 'register' || data.tpl_url === 'login') {
+            socket.emit('plugins.canvasCaptcha.getProblem', function (err, results) {
                 if(!err) {
                     $('#form-text-for-' + results.uuid + ' span').text(results.problem);
                 } else {
