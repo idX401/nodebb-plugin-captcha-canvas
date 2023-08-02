fe847e8b8b2bcf19cf4bec991e55b2779cf36440
idX401 <46228103+idX401@users.noreply.github.com>
2023-08-02 14:28:49 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 $(function() {
     $(window).on('action:ajaxify.end', function (evt, data) {
+        console.log(data);
         if(data.tpl_url === 'register' || data.tpl_url === 'login') {
             socket.emit('plugins.canvasCaptcha.getProblem', function (err, results) {
+                console.log(results);
                 if(!err) {
                     $('#form-text-for-' + results.uuid + ' span').text(results.problem);
                 } else {
